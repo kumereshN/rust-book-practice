5d892319d2795b4724115474f149772d3102c2ee
kumereshN <72869264+kumereshN@users.noreply.github.com>
2022-11-20 16:48:02 +0800

    Delete 05_structs/rectangles/target directory

--- 05_structs/rectangles/target/debug/deps/rectangles.d
+++
@@ -1,5 +0,0 @@
-D:\learning-rust\rust-book\05_structs\rectangles\target\debug\deps\rectangles.exe: src\main.rs
-
-D:\learning-rust\rust-book\05_structs\rectangles\target\debug\deps\rectangles.d: src\main.rs
-
-src\main.rs:
--- 05_structs/rectangles/target/debug/rectangles.d
+++
@@ -1,1 +0,0 @@
-D:\learning-rust\rust-book\05_structs\rectangles\target\debug\rectangles.exe: D:\learning-rust\rust-book\05_structs\rectangles\src\main.rs
