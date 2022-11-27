85ba15beb53a349c0d04c1b0a8ed8016467085bb
kumereshN <kumeresh6@hotmail.com.com>
2022-11-27 16:57:59 +0800

    stopped at chapter 6 match control

--- 06_enums_and_pattern_matching/enums_and_pattern_matching/src/main.rs
+++ 06_enums_and_pattern_matching/enums_and_pattern_matching/src/main.rs
@@ -1,9 +1,6 @@
-enum IpAddrKind {
-    V4(u8, u8, u8, u8),
-    V6(String),
-}
-
 fn main() {
-    let home = IpAddrKind::V4(127, 0, 0, 1);
-    let loopback = IpAddrKind::V6(String::from("::1"));
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+
+    let sum = x + y;
 }
