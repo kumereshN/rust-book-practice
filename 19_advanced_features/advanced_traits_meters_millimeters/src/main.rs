7facacde93cef1232d68e98e41f51ba15dca92f7
kumereshN <kumeresh6@hotmail.com.com>
2023-01-25 07:59:51 +0800

    stopped at Never Type

--- 19_advanced_features/advanced_traits_meters_millimeters/src/main.rs
+++ 19_advanced_features/advanced_traits_meters_millimeters/src/main.rs
@@ -4,7 +4,7 @@ struct Millimeters(u32);
 struct Meters(u32);
 
 impl Add<Meters> for Millimeters {
-    type Output =Millimeters;
+    type Output = Millimeters;
 
     fn add(self, other: Meters) -> Millimeters {
         Millimeters(self.0 + (other.0 * 1000))
